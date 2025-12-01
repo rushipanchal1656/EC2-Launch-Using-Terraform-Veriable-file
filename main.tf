7d2a4a988587e866d501ca72f5a6d513380d1f1c
rushikeshpanchal <rushikeshpanchal196@gmail.com>
2025-12-01 17:35:32 +0530

    Update

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+#USE VAEIABLES WHICH ARE DEFINED IN variables.tf FILE
+resource "aws_instance" "web-server" {
+  ami                = var.ami_id
+  instance_type      = var.instance_type
+  key_name           = var.key_name
+  vpc_security_group_ids = var.security_group_ids
+
+    tags = {
+        Name = "WebServerInstance"
+    }
+}
---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  default     = "ami-0d176f79571d18a8f"   #Assignment of ID 
+}
+
+variable "instance_type" {
+  description = "The type of instance to use"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "The name of the key pair to use for the instance"
+  default     = "Mumbai_server_key"
+}
+
+variable "security_group_ids" {
+  description = "The security group IDs to associate with the instance"
+  type        = list(string)
+  default     = ["sg-02f875775bcb86930"]
+}
+
